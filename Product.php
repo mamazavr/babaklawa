055c6c2658b9b2a1ef75ca1ac15240f3f630e27c
serGAY <116800609+mamazavr@users.noreply.github.com>
2024-01-26 11:27:42 +0200

    Add files via upload

---
+++ Product.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Kyslik\ColumnSortable\Sortable;
+
+class Product extends Model
+{
+    use HasFactory;
+    use HasFactory, Sortable;
+
+    protected $fillable = ['slug', 'title', 'description', 'SKU', 'price', 'new_price', 'quantity', 'thumbnail'];
+
+    public $sortable = ['id', 'title', 'SKU', 'price', 'quantity'];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
+
