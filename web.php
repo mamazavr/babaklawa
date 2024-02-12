c4d2b8bbe63f386a0f659cc21b2327b8e750106c
serGAY <116800609+mamazavr@users.noreply.github.com>
2024-02-12 10:50:42 +0200

    Add files via upload

---
+++ web.php
@@ -0,0 +1,24 @@
+<?php
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::name('admin.')->prefix('admin')->middleware(['role:admin|moderator'])->group(function() {
+    Route::get('dashboard', \App\Http\Controllers\Admin\DashboardController::class)->name('dashboard'); // admin.dashboard
+    Route::resource('categories', \App\Http\Controllers\Admin\CategoriesController::class)
+        ->except(['show']);
+    Route::resource('products', \App\Http\Controllers\Admin\ProductsController::class)
+        ->except(['show']);
+});
